c704546d311b050fd49b3abe1437759366ef2c6c
Elderly Pig <Elderly Pig@ELDERLYPIG>
2023-11-14 21:26:21 +0300

    Добавьте файлы проекта.

---
+++ C ++ лаба 2 задача 2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+// Макрос для вычисления определителя второго порядка
+#define DET2(a, b, c, d) ((a) * (d) - (b) * (c))
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    // Исходные данные
+    int a = 2;
+    int b = 3;
+    int c = 4;
+    int d = 5;
+
+    // Вычисление определителя
+    int determinant = DET2(a, b, c, d);
+
+    // Вывод исходных данных и результата расчета
+    std::cout << "Исходные данные:" << std::endl;
+    std::cout << "a: " << a << std::endl;
+    std::cout << "b: " << b << std::endl;
+    std::cout << "c: " << c << std::endl;
+    std::cout << "d: " << d << std::endl;
+
+    std::cout << "Результат расчета:" << std::endl;
+    std::cout << "Определитель: " << determinant << std::endl;
+
+    return 0;
+}
